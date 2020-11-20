35fe180f4560e83a2ee28a2d85f808903b8e22f3
aaronly1 <49920952+aaronly1@users.noreply.github.com>
2020-11-20 12:06:42 -0600

    Update Login.JS

--- adminportal-master/src/Login.js
+++ adminportal-master/src/Login.js
@@ -1,23 +1,42 @@
 import React from 'react'
-import {BrowserRouter as Router, Switch, Route, NavLink, Link} from 'react-router-dom';
-import {Redirect} from "react-router-dom";
+import {BrowserRouter as Router, Switch, Route, useParams} from 'react-router-dom';
 import './Login.css'; 
-import Register from './Register';
-
+import Support from './Support.js'
+import Finance from './Finance.js'
+import Sales from './Sales.js'
+import HR from './HR.js'
+import Technology from './Tech.js'
 
 function Login() {
  
     return (
+        <Router>
         <div>
            <div className="login-container">
-               <div className="title">LOGIN</div>
+                <h1>Login</h1>
                <form action="">
                    <input type="text" className="email-container" placeholder="Email"/>
                    <input type="text" className="password-container" placeholder="Password"/>
-                   <button className="button">Submit</button>
+                   <button className="button" onClick = "/Support" >Submit</button>
                </form>
            </div>
+        <Switch>
+          <Route path = "/Support" component = {Finance}><Support /></Route>
+          <Route path = "/HR" component = {HR}><HR/></Route>
+          <Route path = "/Sales" component = {Sales}><Sales/></Route>
+          <Route path = "/Technology" component = {Technology}><Technology/></Route>
+        </Switch>
          </div>
+         </Router>
+    )
+}
+
+function Child(){
+let {id} = useParams();
+    return (
+    <div>
+        
+    </div>
     )
 }
 
