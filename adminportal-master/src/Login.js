343df4227dad7d29379004828d095520062f7b9f
daogena <66931735+daogena@users.noreply.github.com>
2020-11-20 12:20:03 -0600

    Add files via upload

--- adminportal-master/src/Login.js
+++ adminportal-master/src/Login.js
@@ -1,42 +1,23 @@
 import React from 'react'
-import {BrowserRouter as Router, Switch, Route, useParams} from 'react-router-dom';
+import {BrowserRouter as Router, Switch, Route, NavLink, Link} from 'react-router-dom';
+import {Redirect} from "react-router-dom";
 import './Login.css'; 
-import Support from './Support.js'
-import Finance from './Finance.js'
-import Sales from './Sales.js'
-import HR from './HR.js'
-import Technology from './Tech.js'
+import Register from './Register';
+
 
 function Login() {
  
     return (
-        <Router>
         <div>
            <div className="login-container">
-                <h1>Login</h1>
+               <div className="title">LOGIN</div>
                <form action="">
                    <input type="text" className="email-container" placeholder="Email"/>
                    <input type="text" className="password-container" placeholder="Password"/>
-                   <button className="button" onClick = "/Support" >Submit</button>
+                   <button className="button">Submit</button>
                </form>
            </div>
-        <Switch>
-          <Route path = "/Support" component = {Finance}><Support /></Route>
-          <Route path = "/HR" component = {HR}><HR/></Route>
-          <Route path = "/Sales" component = {Sales}><Sales/></Route>
-          <Route path = "/Technology" component = {Technology}><Technology/></Route>
-        </Switch>
          </div>
-         </Router>
-    )
-}
-
-function Child(){
-let {id} = useParams();
-    return (
-    <div>
-        
-    </div>
     )
 }
 
