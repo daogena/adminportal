4f08d5fc97de471041ba3fbce61bf06a46abe52e
daogena <66931735+daogena@users.noreply.github.com>
2020-11-20 19:32:21 -0600

    Add files via upload

--- server/routes/user-router.js
+++ server/routes/user-router.js
@@ -5,9 +5,11 @@ const UserCtrl = require('../controllers/user-ctrl')
 const router = express.Router()
 
 router.post('/user', UserCtrl.createLogin)
+router.post('/user/login', UserCtrl.login);
 router.put('/user/:id', UserCtrl.updateLogin)
 router.delete('/user/:id', UserCtrl.deleteLogin)
 router.get('/user/:id', UserCtrl.getLoginById)
 router.get('/user', UserCtrl.getLogin)
+
 
 module.exports = router
